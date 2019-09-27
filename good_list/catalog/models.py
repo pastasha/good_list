f7058ad809da929bb6d506bf7164b1ca81575e6e
Roman Naumtsov <TheFlexMAX@mail.ru>
2019-09-27 20:59:02 +0300

    Миграции #1
    Первая версия миграций добавлена
    Добавлено игнорирование конфигурационных файлов IDE PyCharm

--- good_list/catalog/models.py
+++ good_list/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Модель получающая записи для страницы "список добрых дел"
+class GoodDeedRecord(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=500, null=True)
+    #image = models.ImageField(null=True)
+    typeHelpObj = models.CharField(max_length=50)
+    adress = models.CharField(max_length=100, null=True)
+    linkToWeb = models.CharField(max_length=100, null=True)
+    phoneNumber = models.CharField(max_length=10, null=True)
+    generalDescription = models.TextField(max_length=500, null=True)
+    status = models.CharField(max_length=30, null=True)
+    creationDate = models.DateField()
--- good_list/catalog/urls.py
+++ good_list/catalog/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.conf.urls import url
 
+'''
+    Маршрутизатор перенаправляющий на необходимые страницы
+'''
+
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
 ]
