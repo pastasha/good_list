0b8e93e3c8a69f764aa4a4a053a57ad5f9ead024
Roman Naumtsov <TheFlexMAX@mail.ru>
2019-10-09 20:50:55 +0300

    Изменение моделей и панели админа API

--- good_list/good_api/admin.py
+++ good_list/good_api/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import LegalEntity,Individual
+from django.contrib.auth.models import Group
+
+from .models import HumanRecord
+from .models import OrganizationRecord
+from .models import NeedHelpPost
+from .models import Respondent
+from .models import NewsPost
+from .models import ReportPost
 
 
-admin.site.register(LegalEntity)
-admin.site.register(Individual)
+# Конфигурация сайта админа
+admin.site.site_header = 'Панель суперадмина'
+
+admin.site.register(HumanRecord)
+admin.site.register(OrganizationRecord)
+admin.site.register(NeedHelpPost)
+admin.site.register(Respondent)
+admin.site.register(NewsPost)
+admin.site.register(ReportPost)
+
+admin.site.unregister(Group)
 
