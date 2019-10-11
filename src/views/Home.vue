ed52a7614af43e7ad16f9eb70d4b8b7574f2fe72
Constantinely <43109874+Constantinely@users.noreply.github.com>
2019-10-11 20:04:48 +0300

    наведение главной

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,14 @@
       description="Description from vue-headful"
     />
     <div id="home-start">
-      <img src="@/assets/images/path5445.png" id="home-start-img"/>
+      <img src="@/assets/images/path5445.png" id="home-start-img" v-bind:class="{'img-blur' : hovered}" />
+      <p class="hover-text text-center" v-html="hoverText" v-if="hovered"></p>
       <div id="home-buttons" class="flex-column float-left">
         <div class="btn-wrapper">
-          <b-button squared class="home-btn" to="/want-help">Хочу помочь</b-button>
+          <b-button squared class="home-btn" to="/want-help" @mouseover="butHover()" @mouseleave="butNoHover">Хочу помочь</b-button>
         </div>
         <div class="btn-wrapper">
-          <b-button squared class="home-btn" to="/need-help">Нужна помощь</b-button>
+          <b-button squared class="home-btn" to="/need-help" @mouseover="butHover()" @mouseleave="butNoHover">Нужна помощь</b-button>
         </div>
       </div>
     </div>
@@ -33,8 +34,26 @@ import Header from '@/components/Header.vue'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      hoverText:`
+          Уже много лет одесситы с помощью платформы «Доброе Дело» озеленяют и благоустраивают город! 
+          Помогают продуктами, одеждой, техникой, лекарствами, реанимационным оборудованием… 
+          Помогают жить и спасают жизни! Все они не политики, а обычные жители своего города, которые верят, что для того, чтобы вокруг что-то изменилось, надо не говорить, а делать, не разрушать, а созидать!
+        `,
+      hovered: false,
+    }
+  },
+  methods:{
+    butHover(){
+      this.hovered=true
+    },
+    butNoHover(){
+      this.hovered=false
+    }
+  },
   components: {
-    Header
+    Header,
   }
 }
 </script>
