8387b959beafc1ae874f1b0df3088ebcc3363943
Pastasha <papastasha@gmail.com>
2019-10-14 16:19:45 +0300

    Удаление лишнего

--- good_list/catalog/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.urls import reverse
-
-
-# Create your models here.
-
--- good_list/good_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GoodApiConfig(AppConfig):
-    name = 'good_api'
